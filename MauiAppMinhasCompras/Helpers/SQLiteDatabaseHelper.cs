38a26789261b2df33d5273deabdf1a08e8a3a92f
231024 <231024@CPD02-M01>
2025-02-17 10:59:40 -0300

    .

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -13,34 +13,36 @@ namespace MauiAppMinhasCompras.Helpers
             _conn = new SQLiteAsyncConnection(path);
             _conn.CreateTableAsync<Produto>().Wait();
         }
-    }
-    public Task<int> Insert(Produto p)
-    {
-        return _conn.InsertAsync(p);
-    }
+    
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+     
 
-    public Task<List<Produto>> Update(Produto p)
-    {
-        string sql = "UPDATE Produto " +
-            "SET Descricao=?,Quantidade=?,Preco=? WHERE Id=?";
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto " +
+                "SET Descricao=?,Quantidade=?,Preco=? WHERE Id=?";
 
-        return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
-    }
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
 
-    public Task<int> Delete(int id)
-    {
-        return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
-    }
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
 
-    public Task<List<Produto>> GetAll()
-    {
-        return _conn.Table<Produto>().ToListAsync();
-    }
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
 
-    public Task<List<Produto>> Search(string q)
-    {
-        string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
 
-        return _conn.QueryAsync<Produto>(sql);
-    }
+            return _conn.QueryAsync<Produto>(sql);
+        }
+    } 
 }
