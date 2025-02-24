5a077d508100c6431fdb7c0c4f20ec5b6fa37e54
231024 <231024@CPD03-M18>
2025-02-24 10:54:16 -0300

    .

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,30 @@
+using System.Collections.ObjectModel;
+using AudioToolbox;
+using AVFoundation;
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+        lst_produtos.ItemsSource = lista;
 	}
+
+    protected async override void OnAppearing()
+    {
+        try
+        {
+            lista.Clear();
+            List<Produto> tmp = await App.Db.GetAll();
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex) {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
